fbcaca863b7e53d312913b0e2459b9cf4a5508e5
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-21 00:24:59 -0500

    setup routes

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <div>
+      Cart asd
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Cart from "./Cart"
+import Checkout from "./Checkout"
+import Home from "./Home"
+import Product from "./Product"
+import Login from "./Login"
+import Register from "./Register"
+
+export {
+    Cart,
+    Checkout,
+    Home,
+    Product,
+    Login,
+    Register
+}
