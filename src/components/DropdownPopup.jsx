c18513531a6f1838d4507bbf8e47052cad765bfa
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-23 23:08:41 -0500

    dropdown login button implement

--- src/components/DropdownPopup.jsx
+++ src/components/DropdownPopup.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function DropdownPopup() {
+function DropdownPopup({children}) {
   return (
-    <div>
-      
+    <div className='relative group1'>
+        <button className=''>
+
+        </button>
+        <div
+            className='absolute left-0 mt-2 hidden w-40 bg-white shadow-lg rounded-md group1-hover:block'
+        >
+            {
+                children
+            }
+
+        </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,9 +12,13 @@ function Navbar() {
             <img src={amazonLogo} alt='logo' className='h-15'/>
         </Link>
         <SearchBox/>
-        <div className='mr-3'>
+        <div className='mr-3 group relative'>
           <p className='font-semibold text-sm h-4'>Hello, sign in</p>
           <p className='font-bold'>Account & Lists</p>
+          <div className='absolute right-0 w-80 bg-amber-50 flex-col hidden group-hover:flex rounded-sm'>
+            <Link to="#" className='w-30 bg-amber-300 text-center mx-auto rounded-md my-2'>Sign In</Link>
+            <Link to="#" className='w-full'>Logout</Link>
+          </div>
         </div>
     </navbar>
   )
